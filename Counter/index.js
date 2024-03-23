767eecb697f5b36dfe6aa88c7dbb87bac2d72729
Naveen Gowda MY <117651142+naveengowda7@users.noreply.github.com>
2024-03-23 19:23:36 +0530

    Create index.js

---
+++ Counter/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react'; //to rendor react js components
+import ReactDOM from 'react-dom/client'; // to load this into the web-browser
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root")); //it tells react to control the element
+root.render(
+    <App />
+
+);
+
